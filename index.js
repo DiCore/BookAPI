b940c523d5fe16a287fe7cfc69df9f8830c4a445
stanimirkanchev <32367896+stanimirkanchev@users.noreply.github.com>
2018-02-21 10:20:58 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
+app.get('/api', function (req, res) {
+  res.sendFile('./views/homepage.html', {root: __dirname});
+});
+
+app.get('/books', function (req, res) {
+  res.sendFile('./views/books.html', {root: __dirname});
+});
+
+app.get('/login', function (req, res) {
+  res.sendFile('./views/login.html', {root: __dirname});
+});
+//connect to MongoDB
+mongoose.connect('mongodb://localhost/127.0.0.1:27017]/api');
+var db = mongoose.connection;
+
+//handle mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  // we're connected!
+});
+
+//use sessions for tracking logins
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// serve static files from template
+app.use(express.static(__dirname + './views/login.html'));
+
+// include routes
+var routes = require('./routes/router');
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000!');
+});
